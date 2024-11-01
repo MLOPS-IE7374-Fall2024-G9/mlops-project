fb9c42fe896bf71498419c4a6ea84ac4c00078e6
Rajat Keshri <keshri.r@northeastern.edu>
2024-11-01 12:29:29 -0400

    adding ecryption of dvc credentials

--- setup.sh
+++ setup.sh
@@ -28,4 +28,19 @@ echo "WEATHER_API_KEY=\"820479673a8444f69ac162421242809\"" >> "$ENV_FILE"
 
 echo ".env file setup complete with all configurations."
 
+# Decrypt secrets.json if the encrypted file exists
+ENCRYPTED_FILE="mlops-437516-b9a69694c897.json.enc"
+DECRYPTED_FILE="mlops-437516-b9a69694c897.json"
+DECRYPTION_PASSWORD="mlops-group-9"  # Replace with the actual password or prompt user input
 
+if [ -f "$ENCRYPTED_FILE" ]; then
+    if [ ! -f "$DECRYPTED_FILE" ]; then
+        echo "Decrypting $ENCRYPTED_FILE to $DECRYPTED_FILE..."
+        openssl enc -d -aes-256-cbc -in "$ENCRYPTED_FILE" -out "$DECRYPTED_FILE" -k "$DECRYPTION_PASSWORD"
+        echo "Decryption complete."
+    else
+        echo "$DECRYPTED_FILE already exists. Skipping decryption."
+    fi
+else
+    echo "Encrypted secrets file $ENCRYPTED_FILE not found."
+fi
