e0c8a8adc692f469acf7517b62cdab6c83b72b13
Rajat Keshri <keshri.r@northeastern.edu>
2024-11-01 12:37:45 -0400

    updating the compose file such that dataset is mapped correctly

--- setup.sh
+++ setup.sh
@@ -21,10 +21,23 @@ else
     echo ".env file already exists in the current directory."
 fi
 
-# Add environment variables to the .env file
-echo "Adding environment variables to .env file."
-echo "DEMAND_API_KEY=\"f8tGzRmnyw6dJyy1PyS49REmg1qrT2isvVi8i9mt\"" >> "$ENV_FILE"
-echo "WEATHER_API_KEY=\"820479673a8444f69ac162421242809\"" >> "$ENV_FILE"
+# Set the environment variables
+DEMAND_API_KEY="f8tGzRmnyw6dJyy1PyS49REmg1qrT2isvVi8i9mt"
+WEATHER_API_KEY="820479673a8444f69ac162421242809"
+
+# Add or overwrite DEMAND_API_KEY and WEATHER_API_KEY in .env
+echo "Configuring environment variables in .env file."
+if grep -q "^DEMAND_API_KEY=" "$ENV_FILE"; then
+    sed -i "s/^DEMAND_API_KEY=.*/DEMAND_API_KEY=\"$DEMAND_API_KEY\"/" "$ENV_FILE"
+else
+    echo "DEMAND_API_KEY=\"$DEMAND_API_KEY\"" >> "$ENV_FILE"
+fi
+
+if grep -q "^WEATHER_API_KEY=" "$ENV_FILE"; then
+    sed -i "s/^WEATHER_API_KEY=.*/WEATHER_API_KEY=\"$WEATHER_API_KEY\"/" "$ENV_FILE"
+else
+    echo "WEATHER_API_KEY=\"$WEATHER_API_KEY\"" >> "$ENV_FILE"
+fi
 
 echo ".env file setup complete with all configurations."
 
