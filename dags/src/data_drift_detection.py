5b51a78265c14b0a010e4faf132d3906bbbaf8ba
Nikhil-09 <nikhilsirisala22@gmail.com>
2024-11-04 14:07:14 -0500

    corrections in Drift Detection

--- dags/src/data_drift_detection.py
+++ dags/src/data_drift_detection.py
@@ -4,7 +4,7 @@ from scipy.stats import ks_2samp
 from evidently import ColumnMapping
 from evidently.report import Report
 from evidently.metric_preset import DataDriftPreset
-from data_download import *
+from src.data_download import get_data_from_dvc
  
 class DataDriftDetector:
     def __init__(self, baseline_data: pd.DataFrame, new_data: pd.DataFrame):
@@ -75,35 +75,3 @@ class DataDriftDetector:
         if not self.report:
             raise ValueError("Drift report has not been generated. Call detect_drift_evidently first.")
         return self.report.as_dict()
- 
-
-file_path = get_data_from_dvc('data_raw.csv') #pulling data from DVC
-
-df = pd.read_csv(file_path)
-df = df.sample(frac=1, random_state=42).reset_index(drop=True)
-
-# Convert the 'datetime' column to datetime format
-df['datetime'] = pd.to_datetime(df['datetime'], errors='coerce')
-
-#using the existing file to detect drift - need to connect with the daily data 
-split_point = int(len(df) * 0.7)
-baseline_df = df.iloc[:split_point]
-new_data_df = df.iloc[split_point:]
- 
-# Initialize the detector
-drift_detector = DataDriftDetector(baseline_df, new_data_df)
- 
-# Detect drift using Evidently
-drift_detector.detect_drift_evidently()
-evidently_results = drift_detector.get_results()
-print("Evidently Drift Detection Results:",evidently_results)
- 
-# Detect drift using KS Test
-ks_test_results = drift_detector.detect_drift_ks_test()
-ks_df = pd.DataFrame.from_dict(ks_test_results, orient="index")
-print("KS Test Drift Results:",ks_df)
- 
-# Detect drift using PSI
-psi_results = drift_detector.detect_drift_psi()
-psi_df = pd.DataFrame.from_dict(psi_results, orient="index")
-print("PSI Drift Results:", psi_df)
