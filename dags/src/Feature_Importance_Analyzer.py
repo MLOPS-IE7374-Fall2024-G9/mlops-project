9ddb7117bf90096206fa0dc77abf833afcd8b90a
Nikhil-09 <nikhilsirisala22@gmail.com>
2024-11-12 11:30:59 -0500

    Feature Importance Analysis

---
+++ dags/src/Feature_Importance_Analyzer.py
@@ -0,0 +1,13 @@
+# SHAP: Based on game theory, SHAP assigns each feature an importance value by measuring its contribution to the prediction. It offers consistent feature importance at both the global and local levels.
+# LIME: Perturbs input data and builds an interpretable model locally around a specific prediction to show how features impact that particular outcome.
+
+class FeatureImportanceAnalyzer:
+    def __init__(self, model_path, data_path):
+        pass
+
+    def load_model(self):
+        pass
+
+    def load_data(self):
+        pass
+
