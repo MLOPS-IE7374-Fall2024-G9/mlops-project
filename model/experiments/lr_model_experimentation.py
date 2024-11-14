37248ff40c496460cec5c3d14e2d05ab04224a99
Amogha97 <amoghagadde@gmail.com>
2024-11-14 15:26:05 -0500

    MLflow artifacts setup on GCP

--- model/experiments/lr_model_experimentation.py
+++ model/experiments/lr_model_experimentation.py
@@ -1,46 +1,37 @@
 from datetime import datetime
 import os
-# import mlflow
-# import mlflow.sklearn
-# from mlflow.models import infer_signature
 from urllib.parse import urlparse
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import pandas as pd
 import numpy as np
 from mlflow_utils import *
- 
-# # Setting MLflow tracking URI
-# mlflow.set_tracking_uri("http://localhost:5000")
- 
-# # Experiment name
-# # mlflow.create_experiment("Electricity Demand Prediction_1")
-# # mlflow.set_experiment("Electricity Demand Prediction_1")
-# # mlflow.create_experiment("Electricity Demand Prediction_1", artifact_location="/Users/amoghagadde/Desktop/Amogha/Northeastern/SEM_3/ML_Ops/Project/mlops-project/experiments")
 
-# experiment = mlflow.get_experiment_by_name("Electricity Demand Prediction_1")
-# mlflow.set_experiment("Electricity Demand Prediction_1")
 
-# print("Current artifact location:", experiment.artifact_location)
- 
-# data = pd.read_csv('/Users/akm/Desktop/mlops-project/preprocessed_data.csv')
-# X = data.drop(columns=['value'])
-# y = data[['value']]
 
-# X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.8,test_size=0.2) 
+data= pd.read_csv('/Users/amoghagadde/Desktop/Amogha/Northeastern/SEM_3/ML_Ops/Project/mlops-project/dataset/data/data_preprocess.csv')
+# data = data.dropna()
 
-data= pd.read_csv('/Users/amoghagadde/Desktop/Amogha/Northeastern/SEM_3/ML_Ops/Project/bias_mitigated_data.csv')
-data = data.dropna()
-X = data.drop(columns=['value', 'datetime'])
+X = data.drop(columns=['value', 'datetime', 'zone'])
 y = data[['value']]
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.8,test_size=0.2)
+
+# Label encoding for 'subba_name' - fitting only on training data
+label_encoder = LabelEncoder()
+X_train['subba_name_encoded'] = label_encoder.fit_transform(X_train['subba-name'])
+X_test['subba_name_encoded'] = label_encoder.transform(X_test['subba-name'])
+
+# Drop the original 'subba_name' column
+X_train = X_train.drop(columns=['subba-name'])
+X_test = X_test.drop(columns=['subba-name'])
  
 # Define tags
 tags = {
     "model_name": "Linear Regression",
-    "version": "v2.0",
+    "version": "v3.0",
     # "dataset_version": "v2",
     "purpose": "Model Selection"
 }
